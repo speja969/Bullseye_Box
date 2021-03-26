8bf83b40b89193e6c15872ddc18829b4898e015f
speja969 <62497469+speja969@users.noreply.github.com>
2021-03-26 19:36:29 +0100

    Update DistroTube_dark-theme.sh

--- scripts/DistroTube_dark-theme.sh
+++ scripts/DistroTube_dark-theme.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-# ACTION: Install DistroTube dark-theme
-# INFO: Install DistroTube dark-theme
-# DEFAULT: y
 
 sudo apt update && sudo apt upgrade
 
-default_user=$(logname 2>/dev/null || echo ${SUDO_USER:-${USER}})
-HOME="/home/${default_user}"
-
-mkdir ~/.themes          #ako već ne postoji
-sudo chown $USER:$USER ~/.themes
+mkdir /home/$(logname)/.themes          #ako već ne postoji
+sudo chown $(logname):$(logname) /home/$(logname)/.themes
 sudo chmod 777 ~/.themes
-cd ~/.themes
+cd /home/$(logname)/.themes
 git clone https://gitlab.com/dwt1/dt-dark-theme.git 
